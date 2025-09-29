b43ea7ea623142694527e9980c91867cf637ca3d
fabriziolamberti <41021126+fabriziolamberti@users.noreply.github.com>
2025-09-29 15:22:18 +0200

    Solution developed in class on September 29, 2025

--- room_escape.py
+++ room_escape.py
@@ -1,11 +1,30 @@
+from random import randint
+
 def stampa_griglia(n, pos, uscita):
     """Stampa la griglia con G = giocatore, U = uscita, . = spazio vuoto"""
     # TODO
-
+    for r in range(0,n):
+        for c in range(0,n):
+            if r == pos[0] and c == pos[1]:
+                print("G", end="")
+            elif r == n-1 and c ==n-1:
+                print("U", end="")
+            else:
+                print(".", end="")
+        print()
 
 def muovi(pos, mossa):
     """Aggiorna la posizione in base alla mossa"""
     # TODO
+    if(mossa == 'n'):
+        pos[0] = pos[0]-1
+    if(mossa == 'n'):
+        pos[0] = pos[0]+1
+    if(mossa == 'e'):
+        pos[1] = pos[1]+1
+    if(mossa == 'o'):
+        pos[1] = pos[1]-1
+    return pos
 
 
 def gestisci_livello(livello):
@@ -22,7 +41,35 @@ def gestisci_livello(livello):
     uscita = [n - 1, n - 1]  # posizione uscita
 
     # TODO
+    x = randint(0, n-1)
+    y = randint(0, n-1)
 
+    posizione = [x, y]
+
+    print(f"Livello {livello} Griglia {n}x{n}")
+
+    finito = False
+    uscita = False
+
+    while not finito:
+        stampa_griglia(n, posizione, uscita)
+
+        print("Mossa (n/s/e/o): ", end="")
+        mossaLetta = input()
+        posizione = muovi(posizione, mossaLetta)
+        if(posizione[0] == n-1 and posizione[1] == n-1):
+            uscita = True
+            finito = True
+        if(posizione[0]<0 or posizione[0]>n-1 or posizione[1]<0 or posizione[1]>n-1):
+            uscita = False
+            finito = True
+
+        if(uscita == True):
+            print("Hai raggiunto l'uscita!")
+            return True
+        else:
+            print("GAMEOVER: Sei uscito dalla griglia!")
+            return False
 
 def main():
     print("=== Benvenuto in Room Escape ===")
